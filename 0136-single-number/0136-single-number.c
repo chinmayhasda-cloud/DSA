b1c0f139429bab47047174530465ca350b6fd730
CHINMAY <chinmayhasda@gmail.com>
2026-09-12 17:09:43 +0530

    Time: 1526 ms (5.2%), Space: 9.8 MB (99.5%) - LeetHub

---
+++ 0136-single-number/0136-single-number.c
@@ -0,0 +1,24 @@
+int singleNumber(int* nums, int numsSize) {
+   int count;
+   int n;
+    for(int i=0;i<numsSize;i++){
+        count = 0;
+        for (int j=0;j<numsSize;j++){
+
+
+            
+    if(nums[i]==nums[j])
+     {
+        count++;
+     }
+
+        }
+        if(count == 1){
+         n=nums[i];
+        }
+
+
+
+    }
+    return n;
+}
