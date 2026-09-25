1ea6e3b3a16f8e99291f78cc6e67e8cff5eb6954
CHINMAY <chinmayhasda@gmail.com>
2026-09-25 21:13:59 +0530

    Time: 0 ms (100%), Space: 11.6 MB (9.86%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        vector<int>prefix(gain.size()+1);
+       int max=0;
+        for(int i=1;i<=gain.size();i++){
+            prefix[i]=prefix[i-1]+gain[i-1];
+            if(prefix[i]>max){
+                max=prefix[i];
+            }
+        }
+        return max;
+    }
+};
